cb831c22240c6339fdc84c2bb9fdcab63f7d9f71
Christos Cunning <christoscunn@gmail.com>
2019-11-29 19:25:09 +0000

    update2

--- santa/test.py
+++ santa/test.py
@@ -1,11 +1,11 @@
-#import xmaslist as xl
-#import users_db_functions as userdb
-#import santa_db_functions as santadb
-#import create_user_db as createdb
+import xmaslist as xl
+import users_db_functions as userdb
+import santa_db_functions as santadb
+import create_user_db as createdb
 
-#createdb.createDatabase()
-#santadb.loadNamesFromCSV("names.txt")
-#userdb.printDB()
+createdb.createDatabase()
+santadb.loadNamesFromCSV('names.txt')
+userdb.printDB()
 #shuffle = xl.xmasShuffle(3)
 #shuffle.printList()
 #print("test success!")
