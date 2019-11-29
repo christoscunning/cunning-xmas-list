12064e53e8aabfd14e5fb75acd31327f80bfd1be
Christos Cunning <christoscunn@gmail.com>
2019-11-29 11:28:48 -0800

    testing

--- santa/test.py
+++ santa/test.py
@@ -3,8 +3,11 @@ import users_db_functions as userdb
 import santa_db_functions as santadb
 import create_user_db as createdb
 
+#full path
+filename = '/home/pi/cunning-xmas-site/santa/names.txt'
+
 createdb.createDatabase()
-santadb.loadNamesFromCSV('names.txt')
+santadb.loadNamesFromCSV(filename)
 userdb.printDB()
 #shuffle = xl.xmasShuffle(3)
 #shuffle.printList()
