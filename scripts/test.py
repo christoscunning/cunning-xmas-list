060222ec331e700bc5220faa604b9d548a3b662f
Christos Cunning <christoscunn@gmail.com>
2019-11-29 10:56:10 -0800

    test2

--- scripts/test.py
+++ scripts/test.py
@@ -3,9 +3,9 @@ import users_db_functions as userdb
 import santa_db_functions as santadb
 import create_user_db as createdb
 
-#createdb.createDatabase()
-#santadb.loadNamesFromCSV("names.txt")
-#userdb.printDB()
+createdb.createDatabase()
+santadb.loadNamesFromCSV("names.txt")
+userdb.printDB()
 #shuffle = xl.xmasShuffle(3)
 #shuffle.printList()
 print("test success!")
