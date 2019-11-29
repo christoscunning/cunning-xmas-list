97097c296568e57dc3825350fcdf70bc2ccea8c1
Christos Cunning <christoscunn@gmail.com>
2019-11-29 12:48:58 -0800

    update s

--- app/routes.py
+++ app/routes.py
@@ -19,9 +19,18 @@ def index():
     return render_template('index.html', title='Home', user=user, slist = slist)
 
 #login page
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+	    #get data from form if valid
+        d_username = form.username.data
+		d_remember_me = form.remember_me.data
+		
+		flash('Login requested for user {}, remember_me={}'.format(
+		    d_username, d_remember_me))
+        return redirect('/index')
+
     return render_template('login.html', title='Sign In', form=form)
 
 
